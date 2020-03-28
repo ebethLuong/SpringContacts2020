ef1fb99acd686ea08e2ef162a44a65c8bc212a9e
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-03-28 15:34:56 -0500

    added controller annotation

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -1,6 +1,7 @@
 package dmacc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import dmacc.beans.Contact;
 import dmacc.repository.ContactRepository;
 
+
+@Controller
 public class WebController {
 	
 	@Autowired
